d56343853cb6ed412839d4fd057f34683287b516
Claudia Marano <118067217+ClaudiaMarano@users.noreply.github.com>
2025-09-11 12:24:34 +0200

    Update esercizio iris.py
    Aggiunta output atteso

--- esercizio iris.py
+++ esercizio iris.py
@@ -52,5 +52,32 @@ print(df.head())
 
 
 
+"""
+Output atteso:
+
+Accuracy: 0.967
+
+Classification Report:
+                 precision    recall  f1-score   support
+
+    Iris-setosa      1.000     1.000     1.000        10
+Iris-versicolor      1.000     0.900     0.947        10
+ Iris-virginica      0.909     1.000     0.952        10
+
+       accuracy                          0.967        30
+      macro avg      0.970     0.967     0.967        30
+   weighted avg      0.970     0.967     0.967        30
+
+
+  sepal_length  sepal_width  petal_length  petal_width        class
+0           5.1          3.5           1.4          0.2  Iris-setosa
+1           4.9          3.0           1.4          0.2  Iris-setosa
+2           4.7          3.2           1.3          0.2  Iris-setosa
+3           4.6          3.1           1.5          0.2  Iris-setosa
+4           5.0          3.6           1.4          0.2  Iris-setosa
+
+"""
+
+
 
 
