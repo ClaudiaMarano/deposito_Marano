12671ecddbcb27e97d4262608e15cc10519896ba
Claudia Marano <118067217+ClaudiaMarano@users.noreply.github.com>
2025-09-11 16:26:31 +0200

    Update esercizio_credit_card.py
    Aggiunta output atteso, modifica n_estimators e max_depth per RandomForest

--- esercizio_credit_card.py
+++ esercizio_credit_card.py
@@ -43,7 +43,7 @@ print(classification_report(y_test, tree_weight.predict(X_test)))
 
 ### Modello RandomForestClassifier con bilanciamento delle classi class_weight
 
-forest_weight = RandomForestClassifier(class_weight="balanced", random_state=42)
+forest_weight = RandomForestClassifier(class_weight="balanced", random_state=42, n_estimators=50, max_depth=5)
 
 forest_weight.fit(X_train, y_train)
 
@@ -75,7 +75,7 @@ print(classification_report(y_test, tree_smote.predict(X_test)))
 smote = SMOTE(random_state=42)
 X_train_smote, y_train_smote = smote.fit_resample(X_train, y_train)
 
-forest_smote = RandomForestClassifier(random_state=42)
+forest_smote = RandomForestClassifier(random_state=42, n_estimators=50, max_depth=5)
 
 forest_smote.fit(X_train_smote, y_train_smote)
 
@@ -83,5 +83,67 @@ print("\nRandom Forest con SMOTE")
 print(confusion_matrix(y_test, forest_smote.predict(X_test)))
 print(classification_report(y_test, forest_smote.predict(X_test)))
 
+
+
+"""
+OUTPUT ATTESO:
+
+Class
+0    0.998273
+1    0.001727
+Name: proportion, dtype: float64
+
+Decision Tree con class_weight
+[[56830    34]
+ [   27    71]]
+              precision    recall  f1-score   support
+
+           0       1.00      1.00      1.00     56864
+           1       0.68      0.72      0.70        98
+
+    accuracy                           1.00     56962
+   macro avg       0.84      0.86      0.85     56962
+weighted avg       1.00      1.00      1.00     56962
+
+
+Random Forest con class_weight
+[[56652   212]
+ [   11    87]]
+              precision    recall  f1-score   support
+
+           0       1.00      1.00      1.00     56864
+           1       0.29      0.89      0.44        98
+
+    accuracy                           1.00     56962
+   macro avg       0.65      0.94      0.72     56962
+weighted avg       1.00      1.00      1.00     56962
+
+
+Decision Tree con SMOTE
+[[56758   106]
+ [   20    78]]
+              precision    recall  f1-score   support
+
+           0       1.00      1.00      1.00     56864
+           1       0.42      0.80      0.55        98
+
+    accuracy                           1.00     56962
+   macro avg       0.71      0.90      0.78     56962
+weighted avg       1.00      1.00      1.00     56962
+
+
+Random Forest con SMOTE
+[[56448   416]
+ [   11    87]]
+              precision    recall  f1-score   support
+
+           0       1.00      0.99      1.00     56864
+           1       0.17      0.89      0.29        98
+
+    accuracy                           0.99     56962
+   macro avg       0.59      0.94      0.64     56962
+weighted avg       1.00      0.99      1.00     56962
+
+"""
 
 
